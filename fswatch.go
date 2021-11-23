d5dcf1e18a93d20c680649e1633c8e20b6807a9a
pillar <ymblender@gmail.com>
2021-11-24 05:10:28 +0800

    feat: init

---
+++ fswatch.go
@@ -0,0 +1,47 @@
+package fswatch
+
+import (
+"fmt"
+
+"github.com/fsnotify/fsnotify"
+)
+
+func WatchFiles(path []string, fn func(file string)) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Println("watch error:", err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				// fmt.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					fn(event.Name)
+					// log.Println("modified file:", event.Name)
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				fmt.Println("[error] watcher.Errors:", err)
+			}
+		}
+	}()
+
+	for _, p := range path {
+		err = watcher.Add(p)
+		if err != nil {
+			fmt.Println("[error] watcher.Add:", err)
+		}
+	}
+
+	<-done
+}
